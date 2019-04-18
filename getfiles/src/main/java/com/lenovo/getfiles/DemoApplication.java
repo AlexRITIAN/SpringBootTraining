1c04917a48947cadb24727179c34c2ccf618075b
yuhao5 <yuhao5@lenovo.com>
2019-04-18 15:04:14 +0800

    update

--- getfiles/src/main/java/com/lenovo/getfiles/DemoApplication.java
+++ getfiles/src/main/java/com/lenovo/getfiles/DemoApplication.java
@@ -1,13 +1,46 @@
 package com.lenovo.getfiles;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import com.lenovo.getfiles.core.GetLogFIles;
+import com.lenovo.getfiles.properties.PropertiesListener;
+import com.lenovo.getfiles.properties.PropertiesListenerConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	private static Environment env;
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		// FilePath filePath = new FilePath();
+		SpringApplication application = new SpringApplication(DemoApplication.class);
+		application.addListeners(new PropertiesListener("application.properties"));
+		application.run(args);
+		// System.out.println(PropertiesListenerConfig.getProperty("filepath.activeLogPath"));
+		GetLogFIles logFIles = new GetLogFIles(PropertiesListenerConfig.getProperty("filepath.activeLogPath"),
+				PropertiesListenerConfig.getProperty("filepath.startDate"),
+				PropertiesListenerConfig.getProperty("filepath.endDate"),
+				PropertiesListenerConfig.getProperty("filepath.storePath"));
+		System.out.println("startDate : " + PropertiesListenerConfig.getProperty("filepath.startDate"));
+		System.out.println("endDate : " + PropertiesListenerConfig.getProperty("filepath.endDate"));
+
+		// logFIles.getFiles(PropertiesListenerConfig.getProperty("filepath.activeLogPath"));
+		try {
+			logFIles.run();
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("copy completed.");
+		
 	}
 
 }
---
+++ getfiles/src/main/java/com/lenovo/getfiles/core/GetLogFIles.java
@@ -0,0 +1,76 @@
+package com.lenovo.getfiles.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * GetLogFIles
+ */
+public class GetLogFIles {
+
+    private String activeLogPath;
+    private String startDate;
+    private String endDate;
+    private String storePath;
+
+    public GetLogFIles(String activeLogPath, String startDate, String endDate, String storePath) {
+        this.activeLogPath = activeLogPath;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.storePath = storePath;
+    }
+
+    private void getFiles(String path) throws IOException, ParseException {
+        File file = new File(path);
+        File[] fileArray = file.listFiles();
+        // 遍历目录下的文件和子目录
+        for (int i = 0; i < fileArray.length; i++) {
+            if (fileArray[i].isFile()) {
+                System.out.println(fileArray[i].getPath());
+                copyFile(fileArray[i]);
+            } else {
+                getFiles(fileArray[i].getPath());
+            }
+        }
+    }
+
+    private void copyFile(File socurFile) throws IOException, ParseException {
+        String targetFilePath = socurFile.getPath().replace(activeLogPath, storePath);
+        File targetFile = new File(targetFilePath);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        long startDateL = 0;
+        long endDateL = 0;
+        if(!targetFile.getParentFile().exists()){
+            targetFile.getParentFile().mkdirs();
+        }
+        if(startDate != null && endDate != null && !"".equals(startDate) && !"".equals(endDate)){
+           startDateL = format.parse(startDate).getTime();
+           endDateL = format.parse(endDate).getTime(); 
+           if(socurFile.lastModified() >= startDateL && socurFile.lastModified() <= endDateL ){
+               Files.copy(socurFile.toPath(), targetFile.toPath());
+           }
+        }else if(startDate == null && endDate != null && "".equals(startDate) && !"".equals(endDate)){
+            endDateL = format.parse(endDate).getTime();
+            if(socurFile.lastModified() <= endDateL ){
+                Files.copy(socurFile.toPath(), targetFile.toPath());
+            } 
+        }else if(startDate != null && endDate == null && !"".equals(startDate) && "".equals(endDate)){
+            startDateL = format.parse(startDate).getTime();
+            if(socurFile.lastModified() >= startDateL ){
+                Files.copy(socurFile.toPath(), targetFile.toPath());
+            } 
+        }else{
+            Files.copy(socurFile.toPath(), targetFile.toPath());
+        }
+    }
+
+    public void run() throws IOException, ParseException {
+        if(activeLogPath != null){
+            getFiles(activeLogPath);
+        }
+    }
+}
